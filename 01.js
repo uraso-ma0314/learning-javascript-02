128f2c452b2c71727e2ff0fbc246b1f7d6ebf2fa
urasoma0314@gmail.com <urasoma0314@gmail.com>
2019-06-28 12:26:18 +0900

    6/28 課題

--- 01.js
+++ 01.js
@@ -1,10 +1,13 @@
-// FizzBuzz
-
-// サンプル: "1" という文字列を出力
-console.log("1");
-
-// サンプル: "FizzBuzz" という文字列を出力
-// (process.stdout.write で改行なしの出力ができる)
-process.stdout.write("Fizz");
-process.stdout.write("Buzz");
-console.log();
+for (let i = 1; i < 16; ++i ) {
+   if (i % 3 === 0 && i % 5 === 0){
+      console.log("FizzBuzz")
+  }else if (i % 3 === 0){
+      console.log("Fizz");
+  }else if (i % 5 === 0){
+      console.log("Buzz")
+  }else if (i % 3 === 0 && i % 5 === 0){
+      console.log("FizzBuzz")
+  }else {
+      console.log(i);
+  }
+}
